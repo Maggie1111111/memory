ade05d5e3bd02a8d7ce0185adc941ea0d16bc8ba
Maggie1111111 <margrethe.koller@outlook.com>
2024-12-16 08:44:12 +0100

    litt endring i scale animasjonen

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ function JuleMode() {
     document.getElementById("container").style.backgroundColor = "rgb(183, 4, 4)"
 
     //kort mønster
-    const memoryKortelmt = document.getElementById("memoryKort")
+    
 
-    document.memoryKortelmt.style.backgroundColor = "red"
-    document.memoryKortelmt.style.backgroundImage = "linear-gradient(45deg, white 25%, transparent 25.5%, transparent 50%, white 50.5%, white 75%, transparent 75.5%, transparent)"
-    document.memoryKortelmt.style.backgroundSize = "50px 50px"
-    document.memoryKortelmt.style.boxShadow = "rgb(123, 4, 4)"
+    document.getElementById("memoryKort").style.backgroundColor = "red"
+    document.getElementById("memoryKort").style.backgroundImage = "linear-gradient(45deg, white 25%, transparent 25.5%, transparent 50%, white 50.5%, white 75%, transparent 75.5%, transparent)"
+    document.getElementById("memoryKort").style.backgroundSize = "50px 50px"
+    document.getElementById("memoryKort").style.boxShadow = "rgb(123, 4, 4)"
+
+    //nav, fungerer ikke?
+    document.getElementsById("myNav").style.backgroundColor = "rgb(183, 4, 4)"
 }
