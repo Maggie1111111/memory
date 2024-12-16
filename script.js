eef17c0265cc5372d2280a2655c8be3ea352aabc
Maggie1111111 <margrethe.koller@outlook.com>
2024-12-16 11:17:56 +0100

    nesten fullført julemode

--- script.js
+++ script.js
@@ -9,13 +9,48 @@ function JuleMode() {
     document.getElementById("container").style.backgroundColor = "rgb(183, 4, 4)"
 
     //kort mønster
-    
 
-    document.getElementById("memoryKort").style.backgroundColor = "red"
-    document.getElementById("memoryKort").style.backgroundImage = "linear-gradient(45deg, white 25%, transparent 25.5%, transparent 50%, white 50.5%, white 75%, transparent 75.5%, transparent)"
-    document.getElementById("memoryKort").style.backgroundSize = "50px 50px"
-    document.getElementById("memoryKort").style.boxShadow = "rgb(123, 4, 4)"
 
-    //nav, fungerer ikke?
-    document.getElementsById("myNav").style.backgroundColor = "rgb(183, 4, 4)"
+    //queryselector velger en selector slik som nav
+    document.querySelector('nav').style.backgroundColor = "rgb(183, 4, 4)"
+
+    //home knapp
+    const hjemknappelmt = document.getElementById('hjemknapp')
+
+    hjemknappelmt.style.color = "red"
+
+    hjemknappelmt.addEventListener('mouseover', () => {
+        hjemknappelmt.style.backgroundColor = 'red'
+        hjemknappelmt.style.color = 'white'
+      })
+
+      hjemknappelmt.addEventListener('mouseout', () => {
+        hjemknappelmt.style.backgroundColor = 'white'
+        hjemknappelmt.style.color = "red"
+      })
+
+      //prøv på nytt knapp
+      const prøvpånyttknapp = document.getElementById('restart')
+
+      prøvpånyttknapp.style.color = "red"
+
+      prøvpånyttknapp.addEventListener('mouseover', () => {
+        prøvpånyttknapp.style.backgroundColor = 'red'
+        prøvpånyttknapp.style.color = 'white'
+      })
+
+      prøvpånyttknapp.addEventListener('mouseout', () => {
+        prøvpånyttknapp.style.backgroundColor = 'white'
+        prøvpånyttknapp.style.color = "red"
+      })
 }
+
+ //flippe kortene
+
+ const kort = document.querySelectorAll('.memoryKort');
+
+ function flipKort() {
+ this.classList.toggle('flip');
+ }
+
+ kort.forEach(kortet => kortet.addEventListener('click', flipKort));
