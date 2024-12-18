1e52fa97a224b31e80307beafc21290f7034b89c
Maggie1111111 <margrethe.koller@outlook.com>
2024-12-18 18:28:30 +0100

    fiksa på DOM problemet

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 
 //flippe kortene
-const kort = document.querySelectorAll('.memoryKort');
 
-function flipKort() { this.classList.toggle('flip'); }
+    const kort = document.querySelectorAll('.memoryKort')
 
-kort.forEach(kortet => kortet.addEventListener('click', flipKort));
+    function flipKort() { 
+        this.classList.toggle('flip')
+    }
+
+    kort.forEach(kortet => kortet.addEventListener('click', flipKort))
+
+
 
 //klokke 
-const klokkeElement = document.getElementById("klokke")
+/*const klokkeElement = document.getElementById("klokke")
 
-let klokke = 300 
-let klokkeref = null
+let timerValue = 5 * 60
 
-function tikkKlokke(){ klokke -= 1 
-    klokkeElement.innerHTML = klokke + "sek" 
-    if (klokke <= 0){
+function tikkKlokke() {
+const minutter = Math.floor(timervalue / 60)
+const sekunder = timerValue % 60
+
+const minutterklokke = String(minutter).padStart(2, '0')
+const sekunderklokke = String(sekunder).padStart(2, '0')
+
+klokkeElement.textContent = `${minutterklokke}:${sekunderklokke}`
+
+if (timerValue > 0){
+    timerValue -= 1
+}
+    else{
+        clearInterval(klokkeintervall)
+        alert("tiden er ute!")
     }
-    if (!klokkeref){ klokkeref = setInterval (tikkKlokke, 1000) } }
+}
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const julemodeElement = document.getElementById('juleMode')
+tikkKlokke()
+const klokkeintervall = setInterval(tikkKlokke, 1000)*/
 
-        if (julemodeElement) {
-            julemodeElement.addEventListener('click', function() {
-                document.body.classList.toggle('jule-modus')
-            })
-        }
+const julemodeElement = document.getElementById('juleMode')
+
+if (julemodeElement) {
+    julemodeElement.addEventListener('click', function () {
+        document.body.classList.toggle('jule-modus')
     })
+}
