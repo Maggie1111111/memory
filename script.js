cda640d067d4b7dda7f020d8b1212d01b9f1d24c
Maggie1111111 <margrethe.koller@outlook.com>
2025-01-03 16:52:56 +0100

    gir opp på klokke timer med minutter og sekunder

--- script.js
+++ script.js
@@ -82,31 +82,9 @@
 
 
 //klokke 
-/*const klokkeElement = document.getElementById("klokke")
 
-let timerValue = 5 * 60
 
-function tikkKlokke() {
-const minutter = Math.floor(timervalue / 60)
-const sekunder = timerValue % 60
-
-const minutterklokke = String(minutter).padStart(2, '0')
-const sekunderklokke = String(sekunder).padStart(2, '0')
-
-klokkeElement.textContent = `${minutterklokke}:${sekunderklokke}`
-
-if (timerValue > 0){
-    timerValue -= 1
-}
-    else{
-        clearInterval(klokkeintervall)
-        alert("tiden er ute!")
-    }
-}
-
-tikkKlokke()
-const klokkeintervall = setInterval(tikkKlokke, 1000)*/
-
+//julemodus
 const julemodeElement = document.getElementById('juleMode')
 
 if (julemodeElement) {
